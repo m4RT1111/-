7441f0654043915dee25c1ceaebe32cc2f50d7b5
m4RT1111 <149156406+m4RT1111@users.noreply.github.com>
2023-11-02 22:58:54 +0300

    Update часть4.py

--- Лабораторная работа №1/часть4.py
+++ Лабораторная работа №1/часть4.py
@@ -1,8 +1,9 @@
 users = ['user1','user1','user2', 'user2','user3','user4']
-uniq_grades = {'user1','user1','user2', 'user2','user3','user4'}
-countgrades = len(uniq_grades)
-count = len(users)
-print ("{'Общее количество':" , count , end="")
-print ("," ,"'Уникальные посещения':" , countgrades , end="")
-print ("}")
+amount = {
+    'Общее количество':0,
+    'Уникальные посещения':0
+}
+amount['Общее количество'] = len(users)
+amount['Уникальные посещения'] = len(set(users))
+print(amount)
 # TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
